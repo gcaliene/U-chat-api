bc0e691c83738cd3104e67abd73f7dd9c0c2f222
gerson C <gersoncalienes@gmail.com>
2018-01-14 15:00:24 -0500

    added socket.io with connect and disconnet handlers

--- server/server.js
+++ server/server.js
@@ -1,22 +1,30 @@
-
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const app = express();
 const port = process.env.PORT || 3000;
+const server = http.createServer(app);
+const io = socketIO(server);
 
 const publicPath = path.join(__dirname, '../public'); //this is what you want to provide to the express status middleware
 app.use(express.static(publicPath));
 
+io.on('connection', socket => {
+	console.log('New user connected');
+	socket.on('disconnect', () => {
+		console.log('User disconnected');
+	});
+});
 
-
-app.listen(port, () => {
-  console.log(`Started on port ${port} & the Mongo DB is: ${process.env.MONGODB_URI}`);
+server.listen(port, () => {
+	console.log(
+		`Started on port ${port} & the Mongo DB is: ${process.env.MONGODB_URI}`
+	);
 });
 
 // console.log(__dirname+'/../public'); //this doesn't look clean
-console.log(publicPath);
-
 
 //NOTES
-//__dirname gives you the current folder which would be server
+//__dirname gives you the current folder which would be server.. and Using path.join the second argument would be the public folder.
