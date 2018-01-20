9d0c47e7b029407f2ca160dcdf2887fef4966812
Windows <gersoncalienes@gmail.com>
2018-01-20 13:40:46 -0500

    Added form for messages and show incoming messages

--- public/js/index.js
+++ public/js/index.js
@@ -1,15 +1,38 @@
 const socket = io();
-socket.on('connect', function() {
-	console.log('connected to the server mate');
 
-	// socket.emit('createMessage', {
-	// 	from: 'jenny',
-	// 	text: 'create message is working ?'
-	// });
+socket.on('connect', function() {
+  console.log('connected to the server mate');
 });
 socket.on('disconnect', function() {
-	console.log('Disconnected from server, m8.');
+  console.log('Disconnected from server, m8.');
 });
 socket.on('newMessage', function(message) {
-	console.log('New message:', message);
+  const li = jQuery('<li></li>');
+  li.text(`${message.from}: ${message.text}`);
+  jQuery('#messages').append(li);
 });
+
+jQuery('#message-form').on('submit', function(e) {
+  e.preventDefault();
+  socket.emit(
+    'createMessage',
+    {
+      from: 'User',
+      text: jQuery('[name=message]').val()
+    },
+    function() {}
+  );
+});
+
+// socket.emit(
+//   'createMessage',
+//   {
+//     from: 'js from index.js',
+//     text: 'Hi'
+//   },
+//   function(data) {
+//     console.log('====================================');
+//     console.log('Got it', data);
+//     console.log('====================================');
+//   }
+// );
